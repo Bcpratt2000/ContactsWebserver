079bdbb2296dbde4dd8a95e2a000b0d35ed26748
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2018-05-22 13:53:09 -0600

    i have no idea what i am doing

--- main.js
+++ main.js
@@ -3,31 +3,48 @@ var fs = require('fs');
 var url = require('url');
 
 http.createServer(function(req, res){
-	console.log("New Request" + req.url);
-	if(req.method == 'GET'){
+	console.log("New Request " + req.method  + " " + req.url);
+	switch(req.method){
+		case 'GET':
+			//figure out what file is requested
+			var data = getHtmlFile(url.parse(req.url, true).pathname);
+			//respond
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(data);
+			res.end();
+			break;
 
-		//figure out what file is requested
-		var path = url.parse(req.url, true).pathname;
-		if(path == '/'){
-			path = "index.html";
-		}
-		
-		//read requested file
-		try{
-			data = fs.readFileSync('html/' + path, 'utf8');
-		} catch(error){
-			try{
-				data = fs.readFileSync('html/404.html', 'utf8');
-			} catch(error){
-				data = '<p>404 Error</p>';
-			}
-
+		case 'POST':
+		var data = getHtmlFile("/");
+		//respond
 		res.writeHead(200, {'Content-Type': 'text/html'});
-
 		res.write(data);
-		
 		res.end();
-	}
+				break;
+		}
 }).listen(80);
 
+
+
 console.log("server started");
+
+
+
+function getHtmlFile(path){
+	if(path == '/'){
+		path = "index.html";
+	}
+
+	//read requested file, implement 404 errors
+	var data
+	try{
+		data = fs.readFileSync('html/' + path, 'utf8');
+	} catch(error){
+		try{
+			data = fs.readFileSync('html/404.html', 'utf8');
+		} catch(error){
+			data = '<html><p>404 Error</p></html>';
+		}
+	}
+	return data;
+}
